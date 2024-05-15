644ff3c2efb5c52626df84de64d219c5f829bd64
Md. Maynul Islam <islammaynul82@gmail.com>
2024-05-15 13:15:03 +0600

    fixed footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,28 +1,25 @@
 import logo from '../../assets/Logo.png'
+import { FaFacebook, FaGithub, FaInstagram } from "react-icons/fa";
 const Footer = () => {
     return (
         <footer className="footer p-10 bg-gradient-to-r from-[#1488CC] to-[#2B32B2] text-base-content">
-            <img className='w-40' src={logo} alt="" />
-            <nav className='text-white'>
-                <h6 className="footer-title">Services</h6>
-                <a className="link link-hover">Branding</a>
-                <a className="link link-hover">Design</a>
-                <a className="link link-hover">Marketing</a>
-                <a className="link link-hover">Advertisement</a>
-            </nav>
-            <nav className='text-white'>
-                <h6 className="footer-title">Company</h6>
-                <a className="link link-hover">About us</a>
-                <a className="link link-hover">Contact</a>
-                <a className="link link-hover">Jobs</a>
-                <a className="link link-hover">Press kit</a>
-            </nav>
-            <nav className='text-white'>
-                <h6 className="footer-title">Legal</h6>
-                <a className="link link-hover">Terms of use</a>
-                <a className="link link-hover">Privacy policy</a>
-                <a className="link link-hover">Cookie policy</a>
-            </nav>
+            <div className='flex justify-around w-full'>
+                <img className='w-40' src={logo} alt="" />
+                <div>
+                    <h2 className='text-2xl font-medium mb-2'>Social links</h2>
+                    <div className='flex items-center gap-4 text-2xl'>
+                        <FaFacebook></FaFacebook>
+                        <FaGithub></FaGithub>
+                        <FaInstagram></FaInstagram>
+                    </div>
+                </div>
+                <nav className='flex flex-col'>
+                    <h6 className="footer-title">Legal</h6>
+                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+                    <a className="link link-hover">Cookie policy</a>
+                </nav>
+            </div>
         </footer>
     );
 };
